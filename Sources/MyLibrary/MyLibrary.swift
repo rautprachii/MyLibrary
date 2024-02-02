6b4565eada725e130710740459dc5a96206c4240
fermionmac2022-1 <prachir@fermion.in>
2024-02-02 17:20:53 +0530

    initial commit

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public struct MyLibrary{
+    var text = "Hello, World"
+    
+    public init(){
+        
+    }
+    
+    public func HelloToYou() -> String{
+        return HelloToYou()
+    }
+}
