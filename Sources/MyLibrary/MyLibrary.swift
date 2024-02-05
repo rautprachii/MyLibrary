c7a5d3ce49aef7165b93dee1f89afac4e336ce50
fermionmac2022-1 <prachir@fermion.in>
2024-02-05 17:29:48 +0530

    updated code

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -18,45 +18,21 @@
 import UIKit
 
 public class MyLibrary {
+
     // Singleton instance
     public static let shared = MyLibrary()
 
+    // Closure to handle data return
+    public var onDataReturn: ((String) -> Void)?
+
     private init() {}
 
-    // Present the SDK view controller
+    // Function to present the SDK UI
     public func presentSDK(from viewController: UIViewController) {
-        guard let topViewController = UIApplication.shared.keyWindow?.rootViewController else {
-            return
-        }
-
         let sdkViewController = MySDKViewController()
         sdkViewController.onDataReturn = { [weak self] data in
-            // Handle data returned from SDK in the host application
-            self?.handleDataFromSDK(data)
+            self?.onDataReturn?(data)
         }
-        
-        // Present the SDK on the topmost view controller
-        topViewController.present(sdkViewController, animated: true, completion: nil)
-    }
-
-    // Handle data returned from the SDK
-    private func handleDataFromSDK(_ data: String) {
-        // Process the data returned from the SDK (e.g., show a toast message)
-        print("Data received in host application: \(data)")
-        showToast(message: data)
-    }
-
-    // Dismiss the SDK view controller
-    public func dismissSDK() {
-        // Dismiss the topmost view controller if it is presented modally
-        if let topViewController = UIApplication.shared.keyWindow?.rootViewController,
-           let presentedViewController = topViewController.presentedViewController {
-            presentedViewController.dismiss(animated: true, completion: nil)
-        }
-    }
-
-    // Helper function to show a simple toast message
-    private func showToast(message: String) {
-        // ... (unchanged)
+        viewController.present(sdkViewController, animated: true, completion: nil)
     }
 }
--- Sources/MyLibrary/MySDKViewController.swift
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -1,81 +1,32 @@
-//
-//  File.swift
-//  
-//
-//  Created by Fermion Mac 2022 - 1 on 05/02/24.
-//
-
-import Foundation
 import UIKit
-
 
 public class MySDKViewController: UIViewController {
 
     public var onDataReturn: ((String) -> Void)?
 
-    private let dataLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    
-    public override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("SDK View Controller appeared")
-    }
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        print("View Did Load")
-        // Set up UI
-        view.backgroundColor = .white
 
-        let openSDKButton = UIButton(type: .system)
-        openSDKButton.setTitle("Open SDK", for: .normal)
-        openSDKButton.backgroundColor = UIColor.red
-        openSDKButton.addTarget(self, action: #selector(openSDKButtonTapped), for: .touchUpInside)
-        
+        // Create a button
+        let closeButton = UIButton(type: .system)
+        closeButton.setTitle("Close SDK", for: .normal)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
 
-        let goBackButton = UIButton(type: .system)
-        goBackButton.setTitle("Go back", for: .normal)
-        goBackButton.addTarget(self, action: #selector(goBackButtonTapped), for: .touchUpInside)
-        goBackButton.backgroundColor = .green
+        // Add the button to the view
+        view.addSubview(closeButton)
 
-        // Add UI components
-        view.addSubview(openSDKButton)
-        view.addSubview(goBackButton)
-        view.addSubview(dataLabel)
-
-        // Layout UI components
-//        NSLayoutConstraint.activate([
-//            openSDKButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            openSDKButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
-//
-//            goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
-//
-//            dataLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            dataLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            dataLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            dataLabel.heightAnchor.constraint(equalToConstant: 40)
-//        ])
+        // Layout button
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        closeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
-    @objc private func openSDKButtonTapped() {
-        // Send initial data to host application
+    @objc private func closeButtonTapped() {
+        // Send data back to the host application
         onDataReturn?("Hello")
-    }
 
-    @objc private func goBackButtonTapped() {
-        // Send modified data back to host application
-        onDataReturn?("$sendData World")
+        // Dismiss the SDK
         dismiss(animated: true, completion: nil)
     }
-
-    public func updateDataLabel(withData data: String) {
-        // Update label with received data
-        dataLabel.text = data
-    }
 }
+
