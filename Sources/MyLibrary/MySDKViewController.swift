f941ce9cd19e30b2e4ec4f6d66e8940f972e421d
fermionmac2022-1 <prachir@fermion.in>
2024-02-06 00:03:58 +0530

    Added UILable

--- Sources/MyLibrary/MySDKViewController.swift
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -7,12 +7,12 @@ public class MySDKViewController: UIViewController {
     public var onDataReturn: ((String) -> Void)?
     public var onClose: (() -> Void)?
 
-//    private let dataLabel: UILabel = {
-//        let label = UILabel()
-//        label.textAlignment = .center
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
+    private let dataLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     public override func viewDidLoad() {
         
@@ -20,9 +20,7 @@ public class MySDKViewController: UIViewController {
 
         view.backgroundColor = .white
 
-        
-
-        
+    
         let goBackButton = UIButton(type: .system)
         goBackButton.setTitle("Go Back", for: .normal)
         goBackButton.addTarget(self, action: #selector(goBackButtonTapped), for: .touchUpInside)
@@ -30,25 +28,26 @@ public class MySDKViewController: UIViewController {
         view.addSubview(goBackButton)
         
         
-        goBackButton.translatesAutoresizingMaskIntoConstraints = false
-        goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        let infoLabel = UILabel()
+              infoLabel.text = "Additional Information"
+              infoLabel.textAlignment = .center
+              infoLabel.translatesAutoresizingMaskIntoConstraints = false
+              view.addSubview(infoLabel)
         
-//        view.addSubview(dataLabel)
-//        
-//        dataLabel.translatesAutoresizingMaskIntoConstraints = false
-//        dataLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        dataLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+//        goBackButton.translatesAutoresizingMaskIntoConstraints = false
+//        goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        NSLayoutConstraint.activate([
+                   goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
 
-//        NSLayoutConstraint.activate([
-//           goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
-//
-//            dataLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            dataLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            dataLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            dataLabel.heightAnchor.constraint(equalToConstant: 40)
-//        ])
+                   infoLabel.topAnchor.constraint(equalTo: goBackButton.bottomAnchor, constant: 20),
+                   infoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                   infoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                   infoLabel.heightAnchor.constraint(equalToConstant: 40)
+               ])
     }
 
     @objc private func goBackButtonTapped() {
@@ -60,7 +59,7 @@ public class MySDKViewController: UIViewController {
 
     public func updateDataLabel(withData data: String) {
         // Update label with received data
-       // dataLabel.text = data
+        dataLabel.text = data
     }
 }
 
