813e22d0b5ff54b0b13a5587f201b9da55128720
fermionmac2022-1 <prachir@fermion.in>
2024-02-05 15:19:16 +0530

    added controller

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,4 +1,4 @@
-// The Swift Programming Language
+ //The Swift Programming Language
 
 //public struct MyLibrary{
 //    var text = "Hello, World"
@@ -10,52 +10,73 @@
 //    public func HelloToYou() -> String{
 //        return text
 //    }
-//    
-//  
-//   
 //}
 
-import Foundation
+
+// MySDK.swift
+
 import UIKit
 
-
-//public struct MyLibrary{
-//    public init() {}
-//    
-//    public func openSDK(withData data: String, completion: @escaping (String) -> Void) {
-//          
-//            let processedData = processDataFromSDK(data: data)
-//            completion(processedData)
-//        }
-//
-//        private func processDataFromSDK(data: String) -> String {
-//            return data
-//        }
-//
-//        public func goBack(completion: @escaping (String) -> Void) {
-//            let sendData = "Hello"
-//            completion("\(sendData) World")
-//        }
-//}
-
-
-
-public class MyLibrary {
-
+public class MySDK {
     // Singleton instance
-    public static let shared = MyLibrary()
-
-    // Closure to handle data return
-    public var onDataReturn: ((String) -> Void)?
+    public static let shared = MySDK()
 
     private init() {}
 
-    // Function to present the SDK UI
+    // Present the SDK view controller
     public func presentSDK(from viewController: UIViewController) {
         let sdkViewController = MySDKViewController()
         sdkViewController.onDataReturn = { [weak self] data in
-            self?.onDataReturn?(data)
+            // Handle data returned from SDK in the host application
+            self?.handleDataFromSDK(data)
         }
         viewController.present(sdkViewController, animated: true, completion: nil)
     }
+
+    // Handle data returned from the SDK
+    private func handleDataFromSDK(_ data: String) {
+        // Process the data returned from the SDK (e.g., show a toast message)
+        print("Data received in host application: \(data)")
+        showToast(message: data)
+    }
+
+    // Dismiss the SDK view controller
+    public func dismissSDK() {
+        // Add any cleanup code if needed
+        // For example, dismiss any presented view controllers
+        if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
+            topViewController.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    // Helper function to show a simple toast message
+    private func showToast(message: String) {
+        let toastLabel = UILabel()
+        toastLabel.text = message
+        toastLabel.textColor = .white
+        toastLabel.textAlignment = .center
+        toastLabel.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+
+        if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
+            topViewController.view.addSubview(toastLabel)
+
+            toastLabel.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                toastLabel.leadingAnchor.constraint(equalTo: topViewController.view.leadingAnchor, constant: 20),
+                toastLabel.trailingAnchor.constraint(equalTo: topViewController.view.trailingAnchor, constant: -20),
+                toastLabel.bottomAnchor.constraint(equalTo: topViewController.view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+                toastLabel.heightAnchor.constraint(equalToConstant: 40)
+            ])
+
+            UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: { _ in
+                toastLabel.removeFromSuperview()
+            })
+        }
+    }
 }
+
+
---
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -0,0 +1,72 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fermion Mac 2022 - 1 on 05/02/24.
+//
+
+import Foundation
+import UIKit
+
+
+public class MySDKViewController: UIViewController {
+
+    public var onDataReturn: ((String) -> Void)?
+
+    private let dataLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set up UI
+        view.backgroundColor = .white
+
+        let openSDKButton = UIButton(type: .system)
+        openSDKButton.setTitle("Open SDK", for: .normal)
+        openSDKButton.addTarget(self, action: #selector(openSDKButtonTapped), for: .touchUpInside)
+
+        let goBackButton = UIButton(type: .system)
+        goBackButton.setTitle("Go back", for: .normal)
+        goBackButton.addTarget(self, action: #selector(goBackButtonTapped), for: .touchUpInside)
+
+        // Add UI components
+        view.addSubview(openSDKButton)
+        view.addSubview(goBackButton)
+        view.addSubview(dataLabel)
+
+        // Layout UI components
+        NSLayoutConstraint.activate([
+            openSDKButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            openSDKButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+
+            goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
+
+            dataLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            dataLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            dataLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            dataLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+
+    @objc private func openSDKButtonTapped() {
+        // Send initial data to host application
+        onDataReturn?("Hello")
+    }
+
+    @objc private func goBackButtonTapped() {
+        // Send modified data back to host application
+        onDataReturn?("$sendData World")
+        dismiss(animated: true, completion: nil)
+    }
+
+    public func updateDataLabel(withData data: String) {
+        // Update label with received data
+        dataLabel.text = data
+    }
+}
