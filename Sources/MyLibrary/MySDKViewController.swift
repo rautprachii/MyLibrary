8f0120c65e2d6a0eb448db3cb85b0eabeeede6fb
fermionmac2022-1 <prachir@fermion.in>
2024-02-06 12:37:52 +0530

    updated code

--- Sources/MyLibrary/MySDKViewController.swift
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -8,15 +8,25 @@ public class MySDKViewController: UIViewController {
     public var onClose: (() -> Void)?
   //  var initialData: String?
     
+    public static let shared = MySDKViewController()
 
+      private var labelText: String = ""
     
     private let label: UILabel = {
-           let label = UILabel()
-        label.backgroundColor = .yellow
-        label.translatesAutoresizingMaskIntoConstraints = false
-      //  label.isEditable = false
-           return label
-       }()
+          let label = UILabel()
+          label.translatesAutoresizingMaskIntoConstraints = false
+          label.textAlignment = .center
+          return label
+      }()
+
+    
+//    private let label: UILabel = {
+//           let label = UILabel()
+//        label.backgroundColor = .yellow
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//      //  label.isEditable = false
+//           return label
+//       }()
   
 //    convenience init(initialData: String?) {
 //           self.init(nibName: "MySDKViewController", bundle: nil)
@@ -62,18 +72,22 @@ public class MySDKViewController: UIViewController {
 //        goBackButton.translatesAutoresizingMaskIntoConstraints = false
 //        goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 //        goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//        
-        
-                 
-               
+//
     }
     
+   
     
     public func setText(_ text: String, textColor: UIColor = .black) {
-        label.text = text
-        label.textColor = textColor
+        labelText = text // Store the text value
+               label.text = text
+               label.textColor = textColor
         print("text is \(text)")
     }
+    
+    
+    public func getText() -> String {
+            return labelText
+        }
     
 //    public static func printText(_ text: String) {
 //            print("Text from application: \(text)")
@@ -81,23 +95,24 @@ public class MySDKViewController: UIViewController {
     
 
     @objc private func goBackButtonTapped() {
-        onDataReturn?("Hello World")
+        let textToPass = MySDKViewController.shared.getText()
+           onDataReturn?(textToPass)
         onClose?()
         dismiss(animated: true, completion: nil)
     }
     
     
 
-    public func updateDataLabel(withData data: String) {
-        // Update label with received data
-       // dataLabel.text = data
-        label.text = data
-        print(data)
-    }
+//    public func updateDataLabel(withData data: String) {
+//        // Update label with received data
+//       // dataLabel.text = data
+//        label.text = data
+//        print(data)
+//    }
     
-    func handleDataReturn(data: String) {
-           onDataReturn?(data)
-       }
+//    func handleDataReturn(data: String) {
+//           onDataReturn?(data)
+//       }
     
 }
 
