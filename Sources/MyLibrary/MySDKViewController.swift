bbdf029f12577b15c6646686bbbe80bdf2df725b
fermionmac2022-1 <prachir@fermion.in>
2024-02-08 16:42:43 +0530

    Updated Send OTP request

--- Sources/MyLibrary/MySDKViewController.swift
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -132,42 +132,74 @@ public class MySDKViewController: UIViewController {
 //            }
 //    
     
-    public func sendOTPRequest(mobileNumber: String, emailID: String) {
-           let url = "https://uatselfonboarding.utkarsh.bank/app/send-otp"
+//    public func sendOTPRequest(mobileNumber: String, emailID: String) {
+//           let url = "https://uatselfonboarding.utkarsh.bank/app/send-otp"
+//
+//           let headers: HTTPHeaders = [
+//               "Content-Type": "application/json",
+//               "Request-Type": "app",
+//               "Signature-Value": "7c4eb152d1587afa9e9062a1cf9afe54"
+//           ]
+//
+//           let parameters: [String: Any] = [
+//               "mobile_no": mobileNumber,
+//               "email_id": emailID
+//           ]
+//
+//        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+//                      .responseJSON { response in
+//                          switch response.result {
+//                          case .success(let value):
+//                              if let json = value as? [String: Any] {
+//                                  print("API Response: \(json)")
+//                                  
+//                                  if let message = json["msg"] as? String {
+//                                      self.onDataReturn?(message)
+//                                      print("msg is \(message)")
+//                                      
+//                                  }
+//                              }
+//                          case .failure(let error):
+//                            
+//                              print("API Error: \(error.localizedDescription)")
+//                          }
+//                      }
+//       }
+    
+    
+    public func sendOTPRequest(mobileNumber: String, emailID: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let url = "https://uatselfonboarding.utkarsh.bank/app/send-otp"
 
-           let headers: HTTPHeaders = [
-               "Content-Type": "application/json",
-               "Request-Type": "app",
-               "Signature-Value": "7c4eb152d1587afa9e9062a1cf9afe54"
-           ]
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Request-Type": "app",
+            "Signature-Value": "7c4eb152d1587afa9e9062a1cf9afe54"
+        ]
 
-           let parameters: [String: Any] = [
-               "mobile_no": mobileNumber,
-               "email_id": emailID
-           ]
+        let parameters: [String: Any] = [
+            "mobile_no": mobileNumber,
+            "email_id": emailID
+        ]
 
         AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-                      .responseJSON { response in
-                          switch response.result {
-                          case .success(let value):
-                              if let json = value as? [String: Any] {
-                                  print("API Response: \(json)")
-                                  
-                                  if let message = json["msg"] as? String {
-                                      self.onDataReturn?(message)
-                                      print("msg is \(message)")
-                                      
-                                  }
-                              }
-                          case .failure(let error):
-                            
-                              print("API Error: \(error.localizedDescription)")
-                          }
-                      }
-       }
-    
-    
-    
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    if let json = value as? [String: Any] {
+                        print("API Response: \(json)")
+
+                        if let message = json["msg"] as? String {
+                            completion(.success(message))
+                            print("msg is \(message)")
+                        }
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                    print("API Error: \(error.localizedDescription)")
+                }
+            }
+    }
+
     
  
     
