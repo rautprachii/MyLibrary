18b54e3ced0cfffa1d11f9044fcf83a994aef692
fermionmac2022-1 <prachir@fermion.in>
2024-02-09 11:08:39 +0530

    Added verifyOTP Request

---
+++ Sources/Common/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//
+//
+//  Created by Fermion Mac 2022 - 1 on 09/02/24.
+//
+
+import Foundation
+
+class Constants {
+   
+    static let BaseUrl = "https://uatselfonboarding.utkarsh.bank/app//"
+    static let sendOTPUrl = "send-otp"
+    static let verifyOTPUrl = "verify-otp"
+}
--- Sources/MyLibrary/MySDKViewController.swift
+++ Sources/MyLibrary/MySDKViewController.swift
@@ -4,117 +4,117 @@ import UIKit
 import Alamofire 
 
 public class MySDKViewController: UIViewController {
-
+    
     public var onDataReturn: ((String) -> Void)?
     public var onClose: (() -> Void)?
-  //  var initialData: String?
+    //  var initialData: String?
     
     public static let shared = MySDKViewController()
-
-      private var labelText: String = ""
+    
+    private var labelText: String = ""
     
     private let label: UILabel = {
-          let label = UILabel()
-          label.translatesAutoresizingMaskIntoConstraints = false
-          label.textAlignment = .center
-          return label
-      }()
-
-    
-//    private let label: UILabel = {
-//           let label = UILabel()
-//        label.backgroundColor = .yellow
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//      //  label.isEditable = false
-//           return label
-//       }()
-  
-//    convenience init(initialData: String?) {
-//           self.init(nibName: "MySDKViewController", bundle: nil)
-//           self.initialData = initialData
-//       }
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
     
     
-   
+    //    private let label: UILabel = {
+    //           let label = UILabel()
+    //        label.backgroundColor = .yellow
+    //        label.translatesAutoresizingMaskIntoConstraints = false
+    //      //  label.isEditable = false
+    //           return label
+    //       }()
+    
+    //    convenience init(initialData: String?) {
+    //           self.init(nibName: "MySDKViewController", bundle: nil)
+    //           self.initialData = initialData
+    //       }
+    
+    
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-//        textView.text = initialData
+        //        textView.text = initialData
         
         view.backgroundColor = .white
-
+        
         let goBackButton = UIButton(type: .system)
         goBackButton.setTitle("Go Back", for: .normal)
         goBackButton.addTarget(self, action: #selector(goBackButtonTapped), for: .touchUpInside)
-
-
-       
-                view.addSubview(label)
-                view.addSubview(goBackButton)
-        
-      
-               NSLayoutConstraint.activate([
-                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-                label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                label.heightAnchor.constraint(equalToConstant: 100)
-               ])
-        
-       
-               goBackButton.translatesAutoresizingMaskIntoConstraints = false
-               NSLayoutConstraint.activate([
-                   goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                   goBackButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20)
-               ])
         
         
-      
-       
-//        
-//        goBackButton.translatesAutoresizingMaskIntoConstraints = false
-//        goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//
+        
+        view.addSubview(label)
+        view.addSubview(goBackButton)
+        
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            label.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        
+        goBackButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            goBackButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20)
+        ])
+        
+        
+        
+        
+        //
+        //        goBackButton.translatesAutoresizingMaskIntoConstraints = false
+        //        goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //        goBackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //
     }
     
-   
+    
     
     public func setText(_ text: String, textColor: UIColor = .black) {
         labelText = text // Store the text value
-               label.text = text
-               label.textColor = textColor
+        label.text = text
+        label.textColor = textColor
         print("text is \(text)")
     }
     
     
     public func getText() -> String {
-            return labelText
-        }
+        return labelText
+    }
     
-//    public static func printText(_ text: String) {
-//            print("Text from application: \(text)")
-//        }
+    //    public static func printText(_ text: String) {
+    //            print("Text from application: \(text)")
+    //        }
     
-
+    
     @objc private func goBackButtonTapped() {
-//        let textToPass = MySDKViewController.shared.getText()
-//           onDataReturn?("Hello World")
-//        onClose?()
-//        dismiss(animated: true, completion: nil)
+        //        let textToPass = MySDKViewController.shared.getText()
+        //           onDataReturn?("Hello World")
+        //        onClose?()
+        //        dismiss(animated: true, completion: nil)
         
         
         let textToPass = MySDKViewController.shared.getText()
-            let updatedText = textToPass + " World" 
-            onDataReturn?(updatedText)
-            onClose?()
-            dismiss(animated: true, completion: nil)
+        let updatedText = textToPass + " World"
+        onDataReturn?(updatedText)
+        onClose?()
+        dismiss(animated: true, completion: nil)
         
     }
     
     
-
+    
     public func updateDataLabel(withData data: String) {
         // Update label with received data
-       // dataLabel.text = data
+        // dataLabel.text = data
         label.text = data
         print(data)
     }
@@ -122,32 +122,34 @@ public class MySDKViewController: UIViewController {
     
     
     func handleDataReturn(data: String) {
-           onDataReturn?(data)
-       }
+        onDataReturn?(data)
+    }
     
-
+    
     
     public func sendOTPRequest(mobileNumber: String, emailID: String, completion: @escaping (Result<String, Error>) -> Void) {
         let url = "https://uatselfonboarding.utkarsh.bank/app/send-otp"
-
+        
+        //  let url = Constants.BaseUrl + Constants.sendOTPUrl
+        
         let headers: HTTPHeaders = [
             "Content-Type": "application/json",
             "Request-Type": "app",
             "Signature-Value": "7c4eb152d1587afa9e9062a1cf9afe54"
         ]
-
+        
         let parameters: [String: Any] = [
             "mobile_no": mobileNumber,
             "email_id": emailID
         ]
-
+        
         AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
             .responseJSON { response in
                 switch response.result {
                 case .success(let value):
                     if let json = value as? [String: Any] {
                         print("API Response: \(json)")
-
+                        
                         if let message = json["msg"] as? String {
                             completion(.success(message))
                             print("msg is \(message)")
@@ -159,10 +161,54 @@ public class MySDKViewController: UIViewController {
                 }
             }
     }
-
     
- 
     
+    
+    //    func verifyOTP(){
+    //
+    //    }
+    
+    
+    
+    
+    
+    public func verifyOTPRequest(otp: String, emailOTP: String, consent: String, mobileNumber: String, emailID:String, latitude: String, longitude: String) {
+        let url = "https://uatselfonboarding.utkarsh.bank/app/verify-otp"
+        
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Request-Type": "app",
+            "Signature-Value": "7c4eb152d1587afa9e9062a1cf9afe54"
+        ]
+        
+        let parameters: [String: Any] = [
+            "otp": otp,
+            "email_otp": emailOTP,
+            "consent": consent,
+            "mobile_no": mobileNumber,
+            "email_id": emailID,
+            "latitude": latitude,
+            "longitude": longitude
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    if let json = value as? [String: Any] {
+                        print("API Response: \(json)")
+                        
+                        if let message = json["msg"] as? String {
+                            print("verification OTP is\(message)")
+                          //  self.handleVerificationResponse(message: message)
+                        }
+                    }
+                case .failure(let error):
+                    print("API Error: \(error.localizedDescription)")
+                    // Handle failure
+                }
+            }
+        
+    }
     
 }
-
